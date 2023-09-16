5e543e3198f794bca019fbb575e5832cfe7151c0
cgrabbbit <zeceviczeka3@gmail.com>
2023-09-17 00:16:13 +0200

    update

--- hide_mobile_menu.js
+++ hide_mobile_menu.js
@@ -1,7 +1,7 @@
 const header  = document.getElementById("header");
 
 window.addEventListener('scroll', () => {
-        sideNav.style.top = "-300px";
+        sideNav.style.top = `${sideNavHeight}px`;
         menu.src = "images/menu.png";
-        menuBtn.style.rotate = "0deg x"
+        menuBtn.style.rotate = "rotateX(180deg)"
 });
--- open_mobile_menu.js
+++ open_mobile_menu.js
@@ -1,20 +1,21 @@
-const menuBtn = document.getElementById("mobile_burger_menu")
-const sideNav = document.getElementById("mobile_menu")
-const menu = document.getElementById("menu_icon")
+const menuBtn = document.getElementById("mobile_burger_menu");
+const sideNav = document.getElementById("mobile_menu");
+const menu = document.getElementById("menu_icon");
 
-
+// get height of nav bar dynamically
+const sideNavHeight = -sideNav.offsetHeight;
 // set initial position in css
-sideNav.style.top = "-300px";
+sideNav.style.top = `${sideNavHeight}px`;
 
 menuBtn.onclick = function(){
-    if(sideNav.style.top == "-300px"){
+    if(parseInt(sideNav.style.top) === sideNavHeight){
         sideNav.style.top = "0";
         menu.src = "images/close.png";
-        menuBtn.style.rotate = "180deg x"
+        menuBtn.style.transform = "rotateX(180deg)"
     }
     else{
-        sideNav.style.top = "-300px"
+        sideNav.style.top = `${sideNavHeight}px`;
         menu.src = "images/menu.png";
-        menuBtn.style.rotate = "0deg x"
+        menuBtn.style.transform = "rotateX(0deg)";
     }
 }
